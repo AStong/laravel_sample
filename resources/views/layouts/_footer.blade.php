26b8d147afeaaa95bfc8545b212365c5092f9d17
vagrant <vagrant@homestead>
2017-08-11 09:13:15 +0000

    finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+    <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" alt="">
+    <a href="https://www.baidu.com">
+      百度一下，你就知道！
+    </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ url('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/sample', function () {
     return view('welcome');
 });
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('home');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('/signup', 'UsersController@create')->name('signup');
